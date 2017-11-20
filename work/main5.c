cf869dc8c35d2e8c8cd335991d6bea9bfe46a185
efelzz <vatimofe@gmail.com>
2017-11-20 13:09:23 +0300

    Added srand to get random passwords in task 5

--- work/main5.c
+++ work/main5.c
@@ -1,10 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "task5.h"
 
 int main()
 {
 	char line[9] = { '0' };
+	srand(time(0));
 	for (int i = 0; i < 10; i++)
 	{
 		printf("%s\n", password(line));
