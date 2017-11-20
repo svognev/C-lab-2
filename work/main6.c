f59cac2ded1a1aea4a5d6908f89628ba084e1351
efelzz <vatimofe@gmail.com>
2017-11-20 10:51:18 +0300

    Trying to fix task 6

--- work/main6.c
+++ work/main6.c
@@ -8,12 +8,11 @@ int main()
 	char line[512] = { '0' };
 	printf("Enter a string with extra space characters:\n");
 	fgets(line, 512, stdin);
-	for (int i = 0; i < strlen(line); i++)
+	for (int i = 0; i <= strlen(line); i++)
 	{
 		if (line[i] == '\n')
 			line[i] = '\0';
 	}
-	printf("\nYour string looks like that now:\n%s\n", line);
-	printf("%s\n", clear(line));
+	printf("Your clean string looks like that now:\n%s\n", clear(line));
 	return 0;
 }
