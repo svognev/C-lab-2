57c39efa667c15bf76b3af880449afa44e69335f
Midory7 <33065952+Midory7@users.noreply.github.com>
2017-11-23 00:38:50 +0300

    Create task5.c

---
+++ work/task5.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<time.h>
+
+#define N 9
+
+char * password(char *line)
+{
+	srand(time(0));
+	char ch;
+
+	for (int i = 0; i < N-1; i++)
+	{
+		ch = '\0';
+	    ch = rand() % (122 - 48 + 1) + 48;
+		
+		if (isalnum(ch))
+		{
+			line[i] = ch;
+		}
+	}
+	line[N - 1] = '\0';
+
+	return line;
+}
