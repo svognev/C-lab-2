bc80936cfc38bca50079ccce4660597798b5b2e9
AndreAMolkov <molkov_aa@mail.ru>
2017-11-23 14:03:15 +0300

    fix bag last gap

--- work/main6.c
+++ work/main6.c
@@ -1,20 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define SIZE 300
-#include<stdio.h>
-#include<string.h>
-#include "task6.h"
-int main()
-{
-	int short i=0;
-	char line[SIZE] = {" "};
+ #define SIZE 300
+ #include<stdio.h>
+ #include<string.h>
+ #include "task6.h"
+ int main()
+ {
+	int short i = 0;
+	char line[SIZE] = { " " };
+
 	printf("Enter string with gap :\n");
 	fgets(line, SIZE, stdin);
-	while(line[i]!='\n')//looking '\n'
-	{
+	while (line[i] != '\n')//looking '\n'
+		 {
 		i++;
-	}
+		}
 	
-line[i] = '\0';// chenge '/n' on end of string
+		line[i] = '\0';// chenge '/n' on end of string
+		//char *rez = clear(line);
 	printf("String without gap :\n%s\n", clear(line));
 	return 0;
-}
+	}
