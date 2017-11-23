b20030b1cb370072fc640a8f6a7a54dcfaf40bda
Ksard <ponomarevivansergeevich@gmail.com>
2017-11-24 00:04:28 +0300

    Changes in Task 6

--- work/task6.c
+++ work/task6.c
@@ -1,34 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#define S ' '			// You can set any symbol 
 
 char *clear(char *line) 
 {
 	int i = 0, j = 0;
-	int length = strlen(line);
+	int length = strlen(line);	
 		
-	while (line[length - 2] == ' ')
-	{
-		line[length - 2] = '\0';
-		length--;
-	}
-
 	for (i = 0; i < length; i++)
-	{
-		while (line[i] == ' ' && line[i + 1] == ' ')
+	{		
+		while (line[i] == S && line[i + 1] == S)
 		{				
 			for (j = i + 1; j < length; j++)
 			{
 				line[j] = line[j + 1];	
-			}
-			
-		}		
+			}			
+		}	
+		if (line[length - 1 - i] == S && line[length - i] == '\0')
+			line[length - 1 - i] = '\0';
 	}
 
-	if (line[0] == ' ')
-		for (j = 0; j < length; j++)
-		{
-			line[j] = line[j + 1];
-		}	
+	if (line[0] == S)
+		for (i = 0; i < length; i++)		
+			line[i] = line[i + 1];		
 			
 	return line;
 }
