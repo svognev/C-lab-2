991e94b372bf9c59fca2de191f8e565a3e106c0e
Midory7 <33065952+Midory7@users.noreply.github.com>
2017-11-24 01:38:35 +0300

    Update main7.c

--- work/main7.c
+++ work/main7.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
 #include<time.h>
 
 #define STR 100
@@ -9,10 +10,13 @@ int main()
 {
 	char buf[STR];
 	int count[CH] = { 0 };
+	int len;
 
 	printf("Enter string \n");
 	fgets(buf, STR, stdin);
-
+	len = strlen(buf);
+	buf[len - 1] = '\0';
+	
 	for (int i = 0; buf[i]; i++)
 		count[buf[i]-' ']++;
 
