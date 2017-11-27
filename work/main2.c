88b877754a107596b5ececceb6ff86493adfe95b
PavelBelov <engoldo@gmail.com>
2017-11-27 17:42:12 +0300

    fix 2,3(b25)

--- work/main2.c
+++ work/main2.c
@@ -12,17 +12,16 @@ int main()
 
 	printf("Guess a number from (0 to 100): \n");
 
-	while (1)
+	while (turn(value, secret) != 0)
 	{
 		scanf("%d", &value);
-
-		if (turn(value, secret) == 0)
-			printf("Your Guess was correct! The number is %d \n", secret);
-		else if (turn(value, secret) == 1)
+			
+		if (turn(value, secret) == 1)
 			printf("Guess was too high, try again. \n");
 		else if (turn(value, secret) == -1)
 			printf("Guess was too low, try again\n");
 	}
+	printf("Your Guess was correct! The number is %d \n", secret);
 
 	return 0;
 }
