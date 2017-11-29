2292ff2241e47a27c4713e11cde64968e48dbeb7
Midory7 <33065952+Midory7@users.noreply.github.com>
2017-11-30 00:24:50 +0300

    Update task4.c

--- work/task4.c
+++ work/task4.c
@@ -8,7 +8,6 @@
 
 #define N 20
 
-
 char* process(char* line)
 {
 	char ch;
@@ -16,18 +15,17 @@ char* process(char* line)
 
 	len_buf = strlen(line);
 	
-
 	for (int i = 0; i < len_buf; i++)
 	{
-		if (!isdigit((unsigned char)line[i]))
+		if (isdigit((unsigned char)line[i]))
 		{
-			for (int j = 0; j < i; j++)
+			for (int j = len_buf - 1; j > i; j--)
 			{
-				if (isdigit ((unsigned char) line[j]))
+				if (!isdigit((unsigned char)line[j]))
 				{
-					ch = line[j];
-					line[j] = line[i];
-					line[i] = ch;
+					ch = line[i];
+					line[i] = line[j];
+					line[j] = ch;
 					break;
 				}
 			}
