c9bbb4db4bf58df89b8049fff03ac6f3824ebc80
KseniyaKotrikova <32980589+KseniyaKotrikova@users.noreply.github.com>
2017-12-01 20:35:29 +0300

    Update main7.c

--- work/main7.c
+++ work/main7.c
@@ -19,14 +19,7 @@ int main() {
     
     printf("Input string (use only capital letters):   \n");
     scanf("%s",buf);
-    /*
-    srand((unsigned int)time(0));
-    for(i=0; i<SIZE-1;i++)
-        buf[i]=rand() % ('Z'-'A'+1)+'A';
-    buf[SIZE-1]='\0';
-     */
-    
-    //puts (buf);
+  
     
     i=0;
     while(buf[i] != '\0')
@@ -42,7 +35,7 @@ int main() {
         if(count[i]>0)
             printf("%c-%d\n", i+'A', count[i]);
     }
- //   printf("Max frequence: %d\n", max);
+
     
     
     for(i=0;i<N;i++)
